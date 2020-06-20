9e6664d6f5b653bda2a1b55fad8f43ba4424877c
Aleksander Salnikov <alex@jabbam.net>
2020-06-20 17:47:12 +0300

    phpmailer

---
+++ Back/mail/mail.php
@@ -0,0 +1,12 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$passMainMail = 'U5d-amB-RYn-y8W';
+$Username = 'no-reply@xn--80aaaas3ade3dbfv.xn--p1acf';
+echo 'load ok';
+
+
+?>
